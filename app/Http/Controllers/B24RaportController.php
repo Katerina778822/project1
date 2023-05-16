41b1f293ac06501827aa0b4f3728c28d561a0dfc
Alexandr <shulga.alg@gmail.com>
2023-05-16 19:04:46 +0300

    16/05 updateTime

---
+++ app/Http/Controllers/B24RaportController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\B24Activity;
+use App\Models\B24Raport;
+use App\Models\B24Ring;
+use App\Models\B24Task;
+use App\Models\Company;
+use DateTime;
+use Illuminate\Http\Request;
+
+class B24RaportController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        /*       $D = Company::find(5445);
+        $deals = $D->getClientStatus();// 4-новый; 3-Остывший; 2 - База; 1 - Клиент;
+
+        $start = new DateTime();
+        $end = new DateTime();
+        $start->setTime(0, 0, 0);
+$user_id=1;
+        $Rings = B24Ring::whereBetween('b24_rings.CALL_START_DATE', [$start, $end])
+        ->where('CALL_DURATION','>','10')->get();
+        $Activities = B24Activity::whereBetween('b24_activity.LAST_UPDATED', [$start, $end])
+        ->where('PROVIDER_ID','IMOPENLINES_SESSION')->get();
+        $items = B24Raport::where(, $user_id)->get();
+        //dd($catalog);
+        return view('bitrix24.raport.index', [
+            'items' => $items,
+         //   'items' => $id
+        ]);
+        */
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+    //@fill b24raports table by actual data
+    public function calculatePeriod(DateTime $start, DateTime $end = null)
+    {
+    }
+}
---
+++ database/migrations/2023_05_15_132033_create_b24_raports_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateB24RaportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('b24_raports', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('USER_ID')->nullable()->default(0);
+            $table->unsignedBigInteger('COMPANY_ID')->nullable()->default(0);
+            $table->unsignedBigInteger('LEAD_ID')->nullable()->default(0);
+            $table->unsignedBigInteger('DEAL_ID')->nullable()->default(0);
+            $table->unsignedBigInteger('TASK_ID')->nullable()->default(0);
+            $table->unsignedBigInteger('RING_ID')->nullable()->default(0);
+            $table->unsignedBigInteger('ACTIVITY_ID')->nullable()->default(0);
+            $table->Integer('DEAL_TYPE')->nullable()->default(0);//4-новый; 3-Остывший; 2 - База; 1 - Клиент;
+            $table->Integer('DEAL_STATUS')->nullable()->default(0);//4-выигрыш; 3-прогресс; 2 - перенос; 1 - отказ;
+            $table->date('DATE')->nullable()->default('2023-01-01');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('b24_raports');
+    }
+}
