ba6ccaca6dc8fc772229da7e5c13b28c85bf9cca
Alexandr <shulga.alg@gmail.com>
2023-12-18 11:14:32 +0200

    18.12

--- database/migrations/2023_09_22_164824_branch.php
+++ database/migrations/2023_09_22_164824_branch.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Branch;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -10,16 +11,17 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up()
-    {/**/
+    {/*
         Schema::create('branches', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->unsignedBigInteger('business_id');
             $table->foreign('business_id')->references('id')->on('businesses');
-        });
+        });*/
+
 
         Schema::table('b24_deals', function (Blueprint $table) {
-            $table->unsignedBigInteger('branch_id')->default(1); // Define the branch_id column
+    //        $table->unsignedBigInteger('branch_id')->default(1); // Define the branch_id column
             $table->foreign('branch_id')->references('id')->on('branches');
         });
 
